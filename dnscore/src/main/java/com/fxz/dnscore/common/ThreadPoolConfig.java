df0e65de20634435336dbb857ade22bc5492ca27
fuxiuzhan <fuxiuzhan@163.com>
2022-06-25 12:23:42 +0800

    opts thread monitor

--- dnscore/src/main/java/com/fxz/dnscore/common/ThreadPoolConfig.java
+++ dnscore/src/main/java/com/fxz/dnscore/common/ThreadPoolConfig.java
@@ -4,17 +4,14 @@ import com.fxz.fuled.common.utils.ThreadFactoryNamed;
 import com.fxz.fuled.threadpool.monitor.ThreadPoolRegistry;
 
 import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicLong;
 
 
 /**
  * @author fxz
  */
 public class ThreadPoolConfig {
-    static AtomicLong counter = new AtomicLong(0);
     static final int CORE_THREADS = Math.max(16, Runtime.getRuntime().availableProcessors() * 4);
     static final String THREAD_POOL_PREFIX = "common-";
     static ThreadPoolExecutor singleThreadPoolInstance = null;
